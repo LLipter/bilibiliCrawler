ca8fdc1d83f978c9297e091737046884bf054b3e
LLipter <katsudonn@163.com>
2018-11-13 01:01:21 +0800

    connect to database

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+
 	"github.com/LLipter/bilibili-report/util"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Video struct {
@@ -67,15 +71,55 @@ func sendRequest(addr string) (Info, error) {
 	return info,nil
 }
 
+func Insert(video Video, db *sql.DB) {
+	stmt, err := db.Prepare("INSERT INTO video VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec(
+		video.Aid,
+		video.Status,
+		video.View,
+		video.Dannmaku,
+		video.Reply,
+		video.Favorite,
+		video.Coin,
+		video.Share,
+		video.Now_rank,
+		video.His_rank,
+		video.Support,
+		video.Dislike,
+		video.No_reprint,
+		video.Copyright)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID=%d, affected=%d\n", lastId, rowCnt)
+}
+
 
 
 func main() {
-	info,err := sendRequest("https://api.bilibili.com/archive_stat/stat?aid=2")
+	info, err := sendRequest("https://api.bilibili.com/archive_stat/stat?aid=2")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	util.PrintJson(info)
 	fmt.Println(info)
+
+
+	db, err := sql.Open("mysql", "root:5720@tcp(www.irran.top:3306)/bilibili")
+	defer db.Close()
+	Insert(info.Data, db)
+
 
 }
