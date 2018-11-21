0eed73364fc6374414739917a055bb9f244aa375
LLipter <katsudonn@163.com>
2018-11-21 22:31:55 +0800

    part of acu

--- visual/online.R
+++ visual/online.R
@@ -15,16 +15,19 @@ online.data <- data.frame(ts=as.POSIXct(online.rawdata$ts),
 
 oo <- options(scipen=200)
 online.plot <- ggplot(data=online.data)
-online.plot <- online.plot + geom_line(mapping=aes(x=ts, y=webonline, colour="online user"))
-online.plot <- online.plot + geom_line(mapping=aes(x=ts, y=allcount*30, colour="new video"))
+online.plot <- online.plot + geom_line(mapping=aes(x=ts, y=webonline, colour="Online User"))
+online.plot <- online.plot + geom_line(mapping=aes(x=ts, y=allcount*30, colour="New Video"))
 # online.plot <- online.plot + scale_y_continuous(sec.axis=sec_axis(~.+1000000, name="allcount"))
-online.plot <- online.plot + scale_y_continuous(sec.axis=sec_axis(~./30, name="allcount"))
+online.plot <- online.plot + labs(title="Bilibili Daily Active User")
+online.plot <- online.plot + ylab("Number of online users")
+online.plot <- online.plot + xlab("")
+online.plot <- online.plot + scale_y_continuous(sec.axis=sec_axis(~./30, name="Number of New Video"))
 online.plot <- online.plot + scale_x_datetime(breaks=date_breaks("6 hour"), date_labels="%b %d %H:00")
 online.plot <- online.plot + theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-online.plot <- online.plot + xlab("")
-# online.plot <- online.plot + scale_fill_identity(name='the fill', guide = 'legend',labels = c('m1'))
-# online.plot <- online.plot + scale_colour_manual(name='the colour', values=c('blue'='blue','red'='red'), labels = c('c2','c1'))
-online.plot
+
+
+online.plot <- online.plot + theme(plot.title=element_text(hjust = 0.5))
+ggsave("assets/daily_active_user.png", width=14)
 options(oo)
 
 # close database
