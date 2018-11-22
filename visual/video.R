32c1b3d337d93e2d894bdf889c7c1a6aac3ab5bd
LLipter <katsudonn@163.com>
2018-11-22 12:11:40 +0800

    two figure:

--- visual/video.R
+++ visual/video.R
@@ -6,7 +6,7 @@ Sys.setlocale(locale="UTF-8")
 
 # connect to database
 con <- dbConnect(MySQL(), host="localhost", dbname="bilibili", user="root", password="57575207")
-video.rawdata <- dbGetQuery(con, "SELECT * FROM video WHERE status=0 LIMIT 500000");
+video.rawdata <- dbGetQuery(con, "SELECT * FROM video WHERE status=0");
 
 # view chart
 class <- c("<1k", "1k-10k", "10k-100k", "100k-1M", ">1M")
@@ -43,17 +43,30 @@ ggsave("assets/view_distribution.png")
 
 # pubdate chart
 video.pubdate.data <- as.POSIXct(video.rawdata$pubdate)
-video.pubdate.data.bymonth <- cut(video.pubdate.data, breaks="month")
-video.pubdate.data.split <- split(video.pubdate.data, video.pubdate.data.bymonth)
-video.pubdate.cnt <- data.frame(pubdate=as.POSIXct(names(video.pubdate.data.split)), counts=lengths(video.pubdate.data.split))
+video.pubdate.bymonth <- cut(video.pubdate.data, breaks="month")
+video.pubdate.split <- split(video.pubdate.data, video.pubdate.bymonth)
+video.pubdate.date <- as.POSIXct(names(video.pubdate.split))
+video.pubdate.counts <- lengths(video.pubdate.split)
+video.pubdate.increaserate <- video.pubdate.counts[-1] / video.pubdate.counts[-length(video.pubdate.counts)] - 1
+video.pubdate.increaserate <- append(c(NaN), video.pubdate.increaserate)
+video.pubdate.cumcnt <- cumsum(video.pubdate.counts)
+video.pubdate.plotdata <- data.frame(pubdate=video.pubdate.date, 
+                                    counts=video.pubdate.counts, 
+                                    increaserate=video.pubdate.increaserate,
+                                    cumsum=video.pubdate.cumcnt)
+video.pubdate.plotdata <- video.pubdate.plotdata[-lengths(video.pubdate.plotdata),]                                    
+# video.pubdate.plotdata[which(video.pubdate.plotdata$increaserate==Inf),"increaserate"] <- NaN
 
 oo <- options(scipen=200)
-video.pubdate <- ggplot(data=video.pubdate.cnt)
-video.pubdate <- video.pubdate + geom_line(mapping=aes(x=pubdate, y=counts))
+video.pubdate <- ggplot(data=video.pubdate.plotdata)
 video.pubdate <- video.pubdate + labs(title="Bilibili Monthly New Video")
 video.pubdate <- video.pubdate + ylab("Number of New Video")
 video.pubdate <- video.pubdate + xlab("")
+video.pubdate <- video.pubdate + geom_bar(mapping=aes(x=pubdate, y=cumsum/15), stat="identity", fill="#54BCC3")
+video.pubdate <- video.pubdate + scale_y_continuous(sec.axis=sec_axis(~.*15, name="Number of Total Video", breaks=c(5000000,10000000,15000000,20000000,25000000)))
+video.pubdate <- video.pubdate + geom_line(mapping=aes(x=pubdate, y=counts, color="New Video"))
 video.pubdate <- video.pubdate + scale_x_datetime(breaks=date_breaks("3 month"), date_labels="%Y %b")
+# video.pubdate <- video.pubdate + scale_colour_manual(values=c("#54BCC3","#E87D72"))
 video.pubdate <- video.pubdate + theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
 video.pubdate <- video.pubdate + theme(plot.title=element_text(hjust=0.5))
 ggsave("assets/monthly_new_video.png", width=14, height=7)
@@ -61,5 +74,3 @@ options(oo)
 
 # close database
 dbDisconnect(con)
-
-  
